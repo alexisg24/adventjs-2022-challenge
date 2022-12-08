f0c49799c8884f20758ba7ef2ef20476e2d5a9c9
Alexis Garcia <azoreno2@gmail.com>
2022-12-08 15:47:07 -0400

    :zap: Challenge #8
    Challenge #8 new solution

--- challenges/challenge8/solution8.ts
+++ challenges/challenge8/solution8.ts
@@ -7,3 +7,10 @@ function checkPart(part:string):boolean {
       return str === [...str].reverse().join('')})
     .length >= 1
   }
+
+//Using some
+function checkPart2(part:string):boolean {
+    return [...part].some((item, index) =>{
+      const str:string = part.substring(0, index) + part.substring(index + 1);
+      return str === [...str].reverse().join('')})
+  }
